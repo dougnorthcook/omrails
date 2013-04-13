c6e8909e608082115712c4f1aa657f1c8926d1e2
Douglas North Cook <dougnorthcook@gmail.com>
2013-04-13 12:54:21 -0400

    Add Films Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Omrails::Application.routes.draw do
 
+  resources :films
+
+
   devise_for :views
 
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130413144705) do
+ActiveRecord::Schema.define(:version => 20130413162645) do
+
+  create_table "films", :force => true do |t|
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "pins", :force => true do |t|
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -31,6 +43,12 @@ ActiveRecord::Schema.define(:version => 20130413144705) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "videos", :force => true do |t|
+    t.string   "descrption"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "views", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
