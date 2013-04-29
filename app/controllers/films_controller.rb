dcbcfb89f902a6f1ff17fd2cca9bf8956687425b
Douglas North Cook <dougnorthcook@gmail.com>
2013-04-29 12:51:17 -0400

    Add jQuery Masonry

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -4,7 +4,7 @@ class FilmsController < ApplicationController
   # GET /films
   # GET /films.json
   def index
-    @films = Film.all
+    @films = Film.order("created_at desc")
 
     respond_to do |format|
       format.html # index.html.erb
